d2042a8f5f62c4e2c2af925a008d82f203a5a4e5
minaal-fatima <14074@ait.nsw.edu.au>
2025-02-13 00:28:04 +0000

    Added twig

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php 
-include "header.php";
 
-echo "<h1 style='color:purple; font-family: Inter, sans-serif'>Hello World</h1>";
+require_once "vendor/autoload.php";
+//Create data variables
+$page_title = "Class Project Home Page";
+$greeting = "Welcome to my Website!";
+//Loading the twig template
 
-include "footer.php";
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig  = new \Twig\Environment($loader);
+$template = $twig -> load('page.twig');
+
+//Render the output
+
+echo $template -> render( ['title' => $page_title, 'greeting' => $greeting] ); //Add variables later
+
+// --------- Week 1 -------------
+//include "header.php";
+// echo "<h1 style='color:purple; font-family: Inter, sans-serif'>Hello World</h1>";
+// include "footer.php";
 ?>
-
