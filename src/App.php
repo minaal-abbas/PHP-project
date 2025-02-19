0201c78a3089936d0fadb46af0ae1ef2646b59a1
minaal-fatima <14074@ait.nsw.edu.au>
2025-02-19 23:06:32 +0000

    Added functions to App Class

--- src/App.php
+++ src/App.php
@@ -6,11 +6,27 @@ use Dotenv\Dotenv;
 use \Exception;
 
 class App{
+    # In php, all variables start with '$'
+    protected $config;
+
     # Create Constructor
     public function __construct()
     {
         
     }
+
+    private function loadConfig(){
+        try{
+            $dotenv = Dotenv::createImmutable(__DIR__); 
+            $dotenv->load();
+        } 
+        catch( Exception $exception){
+            $msg = $exception -> getMessage();
+            exit($msg);
+        }
+    }
+
+
 }
 
 ?>
