ca4e07d427496c9edb410ec0f2014e91040f67dd
minaal-fatima <14074@ait.nsw.edu.au>
2025-02-19 22:57:11 +0000

    Created App.php

---
+++ src/App.php
@@ -0,0 +1,17 @@
+<?php
+namespace Syeda\Classproject;
+# Namespace can be found in composer.json
+
+use Dotenv\Dotenv;
+use \Exception;
+
+class App{
+    # Create Constructor
+    public function __construct()
+    {
+        
+    }
+}
+
+?>
+
